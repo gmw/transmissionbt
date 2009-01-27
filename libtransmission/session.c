gmw/transmissionbt

commit a5089328f5e795174b96557ecbd2ac6d9eb64f5c

(trunk libT) another possible fix for the libevent corruption issue -- libevent calls were being invoked from the gui thread at startup via tr_sessionInit()'s call to tr_webInit().  This revision moves those calls into the libtransmission/libevent thread.
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@7806 f4695dd4-2c0a-0410-b89c-da849a56a58e

libtransmission/session.c
@@ -357,6 +357,7 @@ tr_sessionSaveSettings( tr_session * session, const char * configDir, tr_benc * 
 }
 
 static void metainfoLookupRescan( tr_session * );
+static void tr_sessionInitImpl( void * );
 
 tr_session *
 tr_sessionInit( const char  * tag,
@@ -514,10 +515,6 @@ tr_sessionInit( const char  * tag,
     assert( found ); 
     tr_sessionSetSpeedLimit( session, TR_DOWN, i );
     tr_sessionSetSpeedLimitEnabled( session, TR_DOWN, j );
- 
-    /* first %s is the application name
-       second %s is the version number */
-    tr_inf( _( "%s %s started" ), TR_NAME, LONG_VERSION_STRING );
 
     /* initialize the blocklist */
     filename = tr_buildPath( session->configDir, "blocklists", NULL );
@@ -528,15 +525,27 @@ tr_sessionInit( const char  * tag,
     session->isBlocklistEnabled = i; 
     loadBlocklists( session ); 
 
+    session->rpcServer = tr_rpcInit( session, &settings ); 
+
+    tr_bencFree( &settings );
+
+    tr_runInEventThread( session, tr_sessionInitImpl, session );
+    return session;
+}
+static void
+tr_sessionInitImpl( void * vsession )
+{
+    tr_session * session = vsession;
+ 
+    /* first %s is the application name
+       second %s is the version number */
+    tr_inf( _( "%s %s started" ), TR_NAME, LONG_VERSION_STRING );
+
     tr_statsInit( session );
 
     session->web = tr_webInit( session ); 
-    session->rpcServer = tr_rpcInit( session, &settings ); 
 
     metainfoLookupRescan( session );
-
-    tr_bencFree( &settings );
-    return session;
 }
 
 /***

commit 0e051f111eb7e6be954b83fad7178ef4a18b0eb1

(trunk libT) possible fix for the transfers-not-loaded-on-startup issue introduced in r7806
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@7810 f4695dd4-2c0a-0410-b89c-da849a56a58e

libtransmission/session.c
@@ -529,7 +529,11 @@ tr_sessionInit( const char  * tag,
 
     tr_bencFree( &settings );
 
+    session->isWaiting = TRUE;
     tr_runInEventThread( session, tr_sessionInitImpl, session );
+    while( session->isWaiting )
+        tr_wait( 100 );
+
     return session;
 }
 static void
@@ -542,10 +546,9 @@ tr_sessionInitImpl( void * vsession )
     tr_inf( _( "%s %s started" ), TR_NAME, LONG_VERSION_STRING );
 
     tr_statsInit( session );
-
     session->web = tr_webInit( session ); 
-
     metainfoLookupRescan( session );
+    session->isWaiting = FALSE;
 }
 
 /***

libtransmission/session.h
@@ -64,6 +64,7 @@ struct tr_session
     tr_bool                      isProxyEnabled;
     tr_bool                      isProxyAuthEnabled;
     tr_bool                      isClosed;
+    tr_bool                      isWaiting;
     tr_bool                      useLazyBitfield;
 
     tr_bool                      isSpeedLimited[2];