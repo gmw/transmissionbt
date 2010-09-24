gmw/transmissionbt

commit df24480d589fece8c8982a94b33e5eb24f9dcaaf

(trunk gtk) #3554 "alternate speed (turtle) button does not update when toggled via RPC" -- fixed.
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@11215 f4695dd4-2c0a-0410-b89c-da849a56a58e

gtk/main.c
@@ -446,9 +446,35 @@ onRPCChanged( tr_session            * session,
             break;
         }
 
-        case TR_RPC_SESSION_CHANGED:
-            tr_sessionGetSettings( session, pref_get_all( ) );
+        case TR_RPC_SESSION_CHANGED: {
+            int i;
+            tr_benc tmp;
+            tr_benc * newval;
+            tr_benc * oldvals = pref_get_all( );
+            const char * key;
+            tr_bencInitDict( &tmp, 100 );
+            tr_sessionGetSettings( session, &tmp );
+            for( i=0; tr_bencDictChild( &tmp, i, &key, &newval ); ++i )
+            {
+                tr_bool changed;
+                tr_benc * oldval = tr_bencDictFind( oldvals, key );
+                if( !oldval )
+                    changed = TRUE;
+                else {
+                    char * a = tr_bencToStr( oldval, TR_FMT_BENC, NULL );
+                    char * b = tr_bencToStr( newval, TR_FMT_BENC, NULL );
+                    changed = strcmp( a, b ) != 0;
+                    tr_free( b );
+                    tr_free( a );
+                }
+
+                if( changed )
+                    prefschanged( cbdata->core, key, cbdata );
+            }
+            tr_sessionGetSettings( session, oldvals );
+            tr_bencFree( &tmp );
             break;
+        }
 
         case TR_RPC_TORRENT_CHANGED:
         case TR_RPC_TORRENT_MOVED:

commit 1fcdf4c08cbd790856737c0463808c7f1de0f687

(trunk gtk) #3554 "turtle speed button doesn't update when toggled via RPC" -- fix minor bug introduced in r11215
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@11261 f4695dd4-2c0a-0410-b89c-da849a56a58e

gtk/main.c
@@ -452,6 +452,8 @@ onRPCChanged( tr_session            * session,
             tr_benc * newval;
             tr_benc * oldvals = pref_get_all( );
             const char * key;
+            GSList * l;
+            GSList * changed_keys = NULL;
             tr_bencInitDict( &tmp, 100 );
             tr_sessionGetSettings( session, &tmp );
             for( i=0; tr_bencDictChild( &tmp, i, &key, &newval ); ++i )
@@ -469,9 +471,14 @@ onRPCChanged( tr_session            * session,
                 }
 
                 if( changed )
-                    prefschanged( cbdata->core, key, cbdata );
+                    changed_keys = g_slist_append( changed_keys, key );
             }
             tr_sessionGetSettings( session, oldvals );
+
+            for( l=changed_keys; l!=NULL; l=l->next )
+                prefschanged( cbdata->core, key, cbdata );
+
+            g_slist_free( changed_keys );
             tr_bencFree( &tmp );
             break;
         }